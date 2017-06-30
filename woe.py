4f8605be59635c82e33d7b23cb8c7bb9bd99ea59
enzo-quiroz <quenzo@gmail.com>
2017-06-30 14:51:37 -0500

    Rev #00001
    - Se agregó tiempo de ejecución y un esperado del tiempo de ejecución
    - Se agregó la posibilidad de mostrar información en tiempo real para ejecuciones largas y tediosas.

--- woe.py
+++ woe.py
@@ -1,29 +1,44 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime 
 
 __author__ = 'Enzo Quiroz'
 
-
-
 class WoE:
-    def __init__(self, data, x, y, breaks=None):
+    def __init__(self, data, x, y, breaks=None, echo=False, echo_type='none'):
+        # x: debe ser una lista, ejem: x=[col1, col2, col3], x=[col1]
+        
         self.vars = x
         self.x = data[x].copy()
         self.y = data[y].copy()
         self.breaks = breaks
-        self.n = 1
         self.iv = pd.DataFrame()
         self.n = len(x)
-        for i in range(self.n):
-            print(i)
-            #self.iv = self.iv.append(
-            woe(self = self, x = self.x[self.vars[i]], y = self.y, breaks = self.breaks)
-            #woe(self=self, x = self.x, y = self.y, breaks = self.breaks)
+        #creo una lista vacia
+        self.stat = []
+        
+        self.starttime = datetime.now()
+        for i in range(self.n): 
+            _iv, _stat = self.woe(x = self.x[self.vars[i]], y = self.y, breaks = self.breaks)
+            if not np.isnan(_iv):
+                self.iv = self.iv.append({'Variable': self.vars[i], 'IV': _iv}, ignore_index=True)[['Variable','IV']]
+                self.stat.append(_stat)
+            if i==0:
+                self._estimatedtime(self.starttime, datetime.now(), self.n)
+            if echo:
+                print(self.vars[i],': ', _iv)
+            if echo_type == 'stat':
+                print(_stat[['good','bad','obs','PD','woe']], '\n')
+        self.iv = self.iv.sort_values('IV', ascending = False)
+        self.endtime = datetime.now()
+        print('Se ejecutó en: ', str(self.endtime-self.starttime))
     
     def woe(self, x, y, breaks):
         df = pd.DataFrame({"X": x, "Y": y, 'order': np.arange(x.size)})
-        df['labels'] = pd.cut(df['X'], bins = breaks)
-        
+        if df['X'].dtype.name != 'object':
+            df['labels'] = pd.cut(df['X'], bins = breaks)
+        else:
+            df['labels']= df['X'].astype('category')
         col_names = {'count_nonzero':'bad', 'size':'obs'}
         stat = df.groupby('labels')['Y'].agg([np.count_nonzero, np.size]).rename(columns = col_names).copy()
         stat['good'] = stat['obs'] - stat['bad']
@@ -36,10 +51,24 @@ class WoE:
         stat['p_obs'] = stat['obs'] / t_obs
         stat['PD'] = stat['bad'] / t_obs
         
-        stat['woe'] = np.log(stat['p_good'] / stat['p_bad'])*100
+        stat['woe'] = np.log(stat['p_good'] / stat['p_bad'])
 
-        iv_stat = (stat['p_good'] - stat['p_bad'])* stat['woe']/100
-        stat = stat
+        iv_stat = (stat['p_good'] - stat['p_bad'])* stat['woe']
         iv = iv_stat.replace([np.inf, -np.inf], np.nan).dropna().sum()
         
-        return iv
+        return iv, stat
+    
+    def _estimatedtime(self, starttime, endtime, n):
+        print('Se estima que acabará a las: ', str(starttime+(endtime-starttime)*n), '\n')
+        
+    #Aun no implementado del todo, verificar si pd.cut genera ya los percentiles
+    def _getLabels(self, x, breaks):
+        if type(breaks) == 'list':
+            _breaks=breaks
+        else:
+            np.percentile(x, )
+        if x.dtype.name != 'object':
+            df['labels'] = pd.cut(df['X'], bins = _breaks)
+        else:
+            df['labels']= df['X'].astype('category')
+            
