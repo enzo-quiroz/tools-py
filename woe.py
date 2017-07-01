8c85c36f77ebec3d1afa6bc73121d6fd3e8188ea
enzo-quiroz <quenzo@gmail.com>
2017-07-01 12:38:51 -0500

    Top IV numbers
    - Se creo la funcion Top, que devuelve los primeros n elementos que se encuentran entre un ivmin y un ivmax.
    - Se limpió el código para aceptar elementos nulos y trabajar con percentiles

--- woe.py
+++ woe.py
@@ -5,10 +5,11 @@ from datetime import datetime
 __author__ = 'Enzo Quiroz'
 
 class WoE:
-    def __init__(self, data, x, y, breaks=None, echo=False, echo_type='none'):
+    def __init__(self, data, x, y, breaks=None, echo=False, echo_type='none', byzero = 0.001):
         # x: debe ser una lista, ejem: x=[col1, col2, col3], x=[col1]
         
         self.vars = x
+        self.byzero = byzero
         self.x = data[x].copy()
         self.y = data[y].copy()
         self.breaks = breaks
@@ -19,9 +20,9 @@ class WoE:
         
         self.starttime = datetime.now()
         for i in range(self.n): 
-            _iv, _stat = self.woe(x = self.x[self.vars[i]], y = self.y, breaks = self.breaks)
+            _iv, _stat, _obs = self.woe(x = self.x[self.vars[i]], y = self.y, breaks = self.breaks)
             if not np.isnan(_iv):
-                self.iv = self.iv.append({'Variable': self.vars[i], 'IV': _iv}, ignore_index=True)[['Variable','IV']]
+                self.iv = self.iv.append({'Variable': self.vars[i], 'IV': _iv, 'Valid obs': _obs}, ignore_index=True)[['Variable','IV', 'Valid obs']]
                 self.stat.append(_stat)
             if i==0:
                 self._estimatedtime(self.starttime, datetime.now(), self.n)
@@ -35,19 +36,16 @@ class WoE:
     
     def woe(self, x, y, breaks):
         df = pd.DataFrame({"X": x, "Y": y, 'order': np.arange(x.size)})
-        if df['X'].dtype.name != 'object':
-            df['labels'] = pd.cut(df['X'], bins = breaks)
-        else:
-            df['labels']= df['X'].astype('category')
+        df['labels'] = self._getLabels(x, breaks)
         col_names = {'count_nonzero':'bad', 'size':'obs'}
         stat = df.groupby('labels')['Y'].agg([np.count_nonzero, np.size]).rename(columns = col_names).copy()
         stat['good'] = stat['obs'] - stat['bad']
-        t_good = stat['good'].sum()
-        t_bad = stat['bad'].sum()
+        t_good = np.maximum(stat['good'].sum(), self.byzero)
+        t_bad = np.maximum(stat['bad'].sum(), self.byzero)
         t_obs = stat['obs'].sum()
 
-        stat['p_good'] = stat['good'] / t_good
-        stat['p_bad'] = stat['bad'] / t_bad
+        stat['p_good'] = np.maximum(stat['good'] / t_good, self.byzero)
+        stat['p_bad'] = np.maximum(stat['bad'] / t_bad, self.byzero)
         stat['p_obs'] = stat['obs'] / t_obs
         stat['PD'] = stat['bad'] / t_obs
         
@@ -56,7 +54,7 @@ class WoE:
         iv_stat = (stat['p_good'] - stat['p_bad'])* stat['woe']
         iv = iv_stat.replace([np.inf, -np.inf], np.nan).dropna().sum()
         
-        return iv, stat
+        return iv, stat, t_obs
     
     def _estimatedtime(self, starttime, endtime, n):
         print('Se estima que acabará a las: ', str(starttime+(endtime-starttime)*n), '\n')
@@ -64,11 +62,31 @@ class WoE:
     #Aun no implementado del todo, verificar si pd.cut genera ya los percentiles
     def _getLabels(self, x, breaks):
         if type(breaks) == 'list':
-            _breaks=breaks
-        else:
-            np.percentile(x, )
+            _breaks = breaks
+        elif x.dtype.name != 'object':
+            _breaks = np.unique(np.percentile(x[np.logical_not(x.isnull().values)], np.arange(breaks+1)*100/breaks))
         if x.dtype.name != 'object':
-            df['labels'] = pd.cut(df['X'], bins = _breaks)
+            labels = pd.cut(x, bins = _breaks)
         else:
-            df['labels']= df['X'].astype('category')
-            
+            labels = x.astype('category')
+        return (labels)
+    
+    def top(self, n=np.nan, ivmin=np.nan, ivmax=np.nan, part='names'):
+        if np.isnan(n) and np.isnan(ivmin) and np.isnan(ivmax ):
+            print('aji')
+            raise ValueError("No se ha escogido criterio de seleción. Especificar alguno de los parámetros n, ivmin o ivmax")
+        _n = n
+        if n>self.n:
+            _n = self.n
+        if np.isnan(_n):
+            _n = self.n
+        if part == 'names':
+            if np.isnan(ivmin) and np.isnan(ivmax ):
+                return self.iv[0:_n]['Variable'].tolist()
+            else:
+                return self.iv.loc[np.logical_and(
+                        (np.isnan(ivmin) or self.iv['IV']>=ivmin),
+                        (np.isnan(ivmax) or self.iv['IV']<=ivmax))
+                        ][0:_n]['Variable'].tolist()
+        else:
+            return 0
